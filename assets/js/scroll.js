93382c9ead29c6de0a0547b9ae0b16c854696f47
DeBraid <de.braid@gmail.com>
2014-04-10 11:55:35 -0400

    first commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,25 @@
+// $(function() {
+//   $('#navbar a[href*=#]:not([href=#])').click(function ( e )  {
+    
+//     console.log('foobar');
+
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      
+//       if (target.length) {
+        
+//         $('html,body').animate({
+//           e.preventDefault();
+//           scrollTop: target.offset().top
+
+//         }, 5000);
+        
+//         return false;
+//       }
+//     }
+//   });
+// });
+
+console.log('whoo dat');
